c70c486bffe390c3d7e07426ea929d555fb8ea12
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2021-04-25 14:45:23 -0300

    Update Rect de foco.java

--- Projeto 1-2/Rect de foco/Rect de foco.java
+++ Projeto 1-2/Rect de foco/Rect de foco.java
@@ -6,9 +6,12 @@ public void paint (Graphics g) {
         fig.paint(g);
     }
     if (focus != null){
-        g2d.setColor(Color.red);
-        g2d.drawRect(focus.x - 5, focus.y - 5, focus.w + 10, focus.h + 10);
+        if (focus.getClass().equals(Poly.class)){
+            g2d.setColor(Color.red);
+            g2d.drawRect(focus.x - focus.w, focus.y - focus.h, 2*focus.w, 2*focus.h);
+        } else {
+            g2d.setColor(Color.red);
+            g2d.drawRect(focus.x - 5, focus.y - 5, focus.w + 10, focus.h + 10);
+        }
     }
-    focus.paint(g);
-    
 }
