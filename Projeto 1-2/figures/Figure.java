616250e009a780b4091149f506e57491e8d396fa
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2021-05-16 21:04:23 -0300

    Update Figure.java

--- Projeto 1-2/figures/Figure.java
+++ Projeto 1-2/figures/Figure.java
@@ -1,13 +1,15 @@
 package figures;
 
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.awt.geom.Line2D;
 import java.awt.Color;
 
 public abstract class Figure {
     public int x, y;
     public int w, h;
-    public Color contorno;
-    Color fundo;
+    public Color contorno, fundo;
+    
     
     public Figure (int x, int y, int w, int h, Color contorno, Color fundo) {
         this.x = x;
@@ -19,9 +21,21 @@ public abstract class Figure {
     }
     
      public void drag (int dx, int dy) {
-        this.x += dx;
-        this.y += dy;
+        this.x = dx - (this.w)/2;
+        this.y = dy - (this.h)/2;
     }
 
     public abstract void paint (Graphics g);
+
+    public void paintfocus (Graphics g) {
+        g.setColor(Color.red);
+        if (this.getClass().equals(Poly.class)){
+            g.setColor(Color.red);
+            g.drawRect(this.x - this.w, this.y - this.h, 2*this.w, 2*this.h);
+        } else {
+            g.setColor(Color.red);
+            g.drawRect(this.x - 5, this.y - 5, this.w + 10, this.h + 10);
+        }
+        
+    }
 }
