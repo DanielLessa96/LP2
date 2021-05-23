ee5a168394fdea95016c89c6b7eb57690e02d022
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2021-05-23 19:16:39 -0300

    Update Poly.java

--- Projeto 1-2/figures/Poly.java
+++ Projeto 1-2/figures/Poly.java
@@ -17,13 +17,20 @@ public class Poly extends Figure {
           }  
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        this.xPoint = new int[]{x, x + w , x, x - w};
-        this.yPoint = new int[]{y - h, y , y + h, y };
+        this.xPoint = new int[]{x + w/2 , x + w, x + w/2, x};
+        this.yPoint = new int[]{y, y + h/2 , y + h, y + h/2 };
+        if (focused){
+            int[] xfocus = {x + w/2 , (x + w) + 2, x + w/2, x - 2};
+            int[] yfocus = {y - 2 , y + h/2 , (y + h) + 2, y + h/2 };
+            g2d.setColor(Color.red);
+            g2d.draw(new Polygon(xfocus, yfocus, this.npoints));
+        }
         g2d.setColor(this.fundo);
         g2d.fill(new Polygon(this.xPoint, this.yPoint, this.npoints));
         g2d.setColor(this.contorno);
         g2d.draw(new Polygon(this.xPoint, this.yPoint, this.npoints));
     }
+    
 }
