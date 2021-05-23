a18ecedb1b9c6a35fc6a8399fe9ed319a65b6e29
Daniel Lessa <38926235+DanielLessa96@users.noreply.github.com>
2021-05-23 19:15:43 -0300

    Update Ellipse.java

--- Projeto 1-2/figures/Ellipse.java
+++ Projeto 1-2/figures/Ellipse.java
@@ -15,8 +15,12 @@ public class Ellipse extends Figure {
             this.w, this.h, this.x, this.y);
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, Boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
+        if (focused){
+            g2d.setColor(Color.red);
+            g2d.draw(new Ellipse2D.Double(this.x - 2, this.y - 2, this.w + 4, this.h + 4));
+        }
         g2d.setColor(this.fundo);
         g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h ));
         g2d.setColor(this.contorno);
